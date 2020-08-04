53d6b904adf6e2ca60daca32b743f58872f557cd
NC Hammer <nwc@nicchappell.com>
2020-08-04 17:05:06 -0600

    deployment troubleshooting

--- client/assets/js/topic.js
+++ client/assets/js/topic.js
@@ -6,7 +6,7 @@ function loadPage() {
   useIndexedDb("articles", "ArticleStore", "get").then(results => {
     const favorites = results;
     loadArticles().then(data => {
-      const mappedData = data.map(article => {
+      const mappedData = data && data.map(article => {
         article.favorite = false;
         favorites.forEach(fav => {
           if (article._id === fav._id) {
